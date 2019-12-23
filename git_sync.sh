6f687639cc3f3f14be000d4137b35e6e5dc2d5b1
Greg Sharples <23085622+heisengerg@users.noreply.github.com>
2019-12-23 11:46:03 +0000

    Add dockerfile and git_sync.sh

---
+++ git_sync.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cp -rL /keys/* ~/.ssh/
+chmod 600 ~/.ssh/*
+
+rm -r /git
+mkdir /git
+git clone -b $GIT_BRANCH $GIT_REPO /git
+cd /git
+
+while true; do
+    git pull
+    sleep $INTERVAL
+done
